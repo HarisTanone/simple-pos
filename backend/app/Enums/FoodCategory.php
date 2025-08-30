951cef296b4237852b0d09fefe121b1f5b827146
harist-hwg <haris.tanone@hwgroup.id>
2025-08-30 09:28:55 +0700

    first commit

---
+++ backend/app/Enums/FoodCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class FoodCategory extends Enum
+{
+    const MAKANAN = 1;
+    const MINUMAN = 2;
+}
---
+++ backend/app/Enums/UserRole.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class UserRole extends Enum
+{
+    const PELAYAN = 1;
+    const KASIR = 2;
+}
